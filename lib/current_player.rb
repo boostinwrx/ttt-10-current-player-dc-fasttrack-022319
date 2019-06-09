7a35a575a41bcc9fe458df52c00e74265d30facb
ANTONIO J ABRAHAM <tony.calablava@gmail.com>
2019-06-09 16:52:14 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,9 +1,9 @@
 def turn_count(board_array)
   counter = 0
-board_array.each do |num|
-  if(num == "X" | num == "O")
-    counter = counter +1
+  board_array.each do |num|
+      if(num == "X" | num == "O")
+      counter = counter +1
+      end
   end
-end
 
 end
